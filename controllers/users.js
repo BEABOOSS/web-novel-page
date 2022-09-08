6fb7ba88e647ee4570ffc9c86f1d2536a0a017fc
BEABOOSS <72757983+BEABOOSS@users.noreply.github.com>
2022-09-08 13:38:37 -0400

    removed img

--- controllers/users.js
+++ controllers/users.js
@@ -20,7 +20,7 @@ module.exports.register = async (req, res, next) => {
 };
 
 module.exports.renderLogin = (req, res) => {
-	res.render("user/login");
+	res.render("users/login");
 };
 
 module.exports.login = (req, res) => {
