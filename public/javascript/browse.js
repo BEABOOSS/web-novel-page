1c2db6099cb1af8afbcd33bd11469fcfb69b1592
BEABOOSS <72757983+BEABOOSS@users.noreply.github.com>
2022-09-13 13:32:39 -0400

    finished for now the sliders

--- public/javascript/browse.js
+++ public/javascript/browse.js
@@ -1,31 +1,31 @@
 let slider = tns({
-    container : ".sliders",
-    "slideBy" : 1, 
-    "speed" : 400,
-    "nav": false,
-    controlsContainer: "#controls",
-    prevButton: ".previous",
-    nextButton: ".next",
-    responsive: {
-        1600: {
-            items: 5,
-            gutter: 15
-        },
-        1200: {
-            items: 5,
-            gutter: 10
-        },
-        1024: {
-            items: 4,
-            gutter: 15
-        },
-        768: {
-            items: 3,
-            gutter: 5
-        },
-        480: {
-            items: 2,
-            gutter: 10
-        }
-    }
-})
+	container: ".sliders",
+	slideBy: 1,
+	speed: 400,
+	nav: false,
+	controlsContainer: "#controls",
+	prevButton: ".previous",
+	nextButton: ".next",
+	responsive: {
+		1600: {
+			items: 5,
+			gutter: 15,
+		},
+		1200: {
+			items: 5,
+			gutter: 10,
+		},
+		1024: {
+			items: 4,
+			gutter: 15,
+		},
+		768: {
+			items: 3,
+			gutter: 5,
+		},
+		480: {
+			items: 2,
+			gutter: 10,
+		},
+	},
+});
